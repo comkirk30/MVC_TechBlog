78985e54fc4083bd7923e9b25e5a034089cab650
Courtney <courtneymkirk@hotmail.com>
2022-05-06 14:16:59 -0400

    update user routes again, adding code

--- controllers/api/user.routes.js
+++ controllers/api/user.routes.js
@@ -48,3 +48,41 @@ router.get("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that particular email address' });
+          return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+          res.status(400).json({ message: 'Wrong password' });
+          return;
+        }
